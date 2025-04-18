a691dbee0705e8735e6e8ee15ab4e9245767946a
tr <2500512459@qq.com>
2025-04-18 17:33:39 +0800

    代码优化

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -11,36 +11,38 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody rb;
     private Animator anim;
+    private float h;
+    private float v;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
     }
 
+    private void Update()
+    {
+        // 获取输入
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        // 更新动画状态
+        MoveAnimation(h, v);
+    }
+
     private void FixedUpdate()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-
-        //移动
+        // 移动
         Move(h, v);
 
-        //旋转
+        // 旋转
         Turning();
-
-        //移动动画
-        MoveAnimation(h, v);
-
-
     }
 
     private void MoveAnimation(float h, float v)
     {
         bool isMove = h != 0 || v != 0;
-        if (isMove)
-            anim.SetBool("IsMove", true);
-        else
-            anim.SetBool("IsMove", false);
+        anim.SetBool("IsMove", isMove);
     }
 
     private void Move(float h, float v)
@@ -49,18 +51,18 @@ public class PlayerMovement : MonoBehaviour
         movementV3 = movementV3.normalized * speed * Time.deltaTime;
         rb.MovePosition(transform.position + movementV3);
     }
+
     private void Turning()
     {
-        Ray CameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);//获取鼠标位置
-
-        RaycastHit groundHit;//射线检测
-
+        Ray CameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit groundHit;
         int groundLayer = LayerMask.GetMask("Ground");
+        
         if (Physics.Raycast(CameraRay, out groundHit, 100, groundLayer))
         {
-            Vector3 targetV3 = groundHit.point - transform.position;// 获取射线碰撞点与玩家之间的向量
+            Vector3 targetV3 = groundHit.point - transform.position;
             targetV3.y = 0;
-            Quaternion quaternion = Quaternion.LookRotation(targetV3);// 获取目标向量所对应的旋转
+            Quaternion quaternion = Quaternion.LookRotation(targetV3);
             rb.MoveRotation(quaternion);
         }
     }
