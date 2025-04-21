62c874d14028234957a657d94748ba02f1c58a76
Migu <2500512459@qq.com>
2025-04-21 20:17:44 +0800

    增加子弹射击

---
+++ Assets/Scripts/Player/MyPlayerShooting.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyPlayerShooting : MonoBehaviour
+{
+    private AudioSource gunAudio;
+    private Light gunLight;
+    private LineRenderer gunLine;
+    private ParticleSystem gunParticle;
+
+    public float shootTime; // 发射子弹时间间隔
+    public float effectDelayTime;  // 特效延迟时间
+
+    private float currentShootTime; // 当前时间间隔
+
+    private void Awake()
+    {
+        gunAudio = GetComponent<AudioSource>();
+        gunLight = GetComponentInChildren<Light>();
+        gunLine = GetComponentInChildren<LineRenderer>();
+        gunParticle = GetComponentInChildren<ParticleSystem>();
+    }
+    private void Update()
+    {
+        currentShootTime += Time.deltaTime;
+        if (currentShootTime >= shootTime)
+        {
+            if (Input.GetButton("Fire1"))
+            {
+                currentShootTime = 0f;
+                // 发射子弹
+                Shoot();
+            }
+        }
+
+        if (currentShootTime >= shootTime * effectDelayTime)
+        {
+            gunLight.enabled = false;
+            gunLine.enabled = false;
+        }
+    }
+
+    private void Shoot()
+    {
+        //Debug.Log(DateTime.Now.ToString("HH::mm::ss::ff"));
+        gunAudio.Play();//播放枪声
+
+        gunLight.enabled = true;//枪光亮
+
+        gunParticle.Play(); //播放粒子特效
+
+        //从枪口发射射线 获取射线命中的物体
+        gunLine.SetPosition(0, transform.position);
+        gunLine.SetPosition(1, transform.position + transform.forward * 100);
+        gunLine.enabled = true;
+    }
+}
